fccc17008753ebe804f6ea9b218b098e89a12377
kinzoku-dev <kinzokudev4869@gmail.com>
2023-11-15 18:43:06 -0600

    feat: added yazi nushell wrapper

--- modules/nixos/apps/nushell/default.nix
+++ modules/nixos/apps/nushell/default.nix
@@ -55,6 +55,16 @@ in {
             nix shell ($packages | each {|s| $"nixpkgs#($s)"})
 
         }
+
+        def-env ya [] {
+            let tmp = (mktemp -t "yazi-cwd.XXXXX")
+                yazi --cwd-file $tmp
+                let cwd = (cat -- $tmp)
+                if $cwd != "" and $cwd != $env.PWD {
+                    cd $cwd
+                }
+            rm -f $tmp
+        }
       '';
     };
   };
