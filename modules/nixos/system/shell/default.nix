682f3710fd153368a6e8c99fda339093b0b92ca2
kinzoku-dev <kinzokudev4869@gmail.com>
2023-11-17 12:21:34 -0600

    change: edit starship prompt

--- modules/nixos/system/shell/default.nix
+++ modules/nixos/system/shell/default.nix
@@ -32,46 +32,8 @@ in {
       enable = true;
       enableZshIntegration = true;
       enableNushellIntegration = true;
-      settings = {
-        format = ''
-          [//=](bold blue) $username$hostname [<>](bold blue) $directory $fill ($nix_shell)$custom
-          [|>](bold blue) '';
-
-        username = {
-          format = "[$user]($style)";
-          show_always = true;
-        };
-
-        hostname = {
-          format = "[@$hostname]($style)";
-          style = "bold green";
-          ssh_only = false;
-        };
-        custom = {
-          nix_inspect = let
-            excluded = [
-              "kitty"
-              "imagemagick"
-              "ncurses"
-              "user-environment"
-              "pciutils"
-              "binutils-wrapper"
-            ];
-          in {
-            disabled = false;
-            when = "test -z $IN_NIX_SHELL";
-            command = "${(lib.getExe pkgs.nebula.nix-inspect)} ${(lib.concatStringsSep " " excluded)}";
-            format = "[($output <- )$symbol]($style) ";
-            symbol = " ";
-            style = "bold blue";
-          };
-        };
-        fill = {
-          symbol = " ";
-          disabled = false;
-        };
-      };
     };
+    home.configFile."starship.toml".source = ./starship.toml;
 
     environment.shellAliases = {
       ".." = "cd ..";
