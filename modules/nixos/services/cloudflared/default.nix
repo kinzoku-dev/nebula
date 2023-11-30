32e269cae11064a2c48d02142eadd9f47fc61237
kinzoku-dev <kinzokudev4869@gmail.com>
2023-11-30 16:00:00 -0600

    fixup! feat: add cloudflared

--- modules/nixos/services/cloudflared/default.nix
+++ modules/nixos/services/cloudflared/default.nix
@@ -19,30 +19,28 @@ in {
       enable = true;
       user = "kinzoku";
       tunnels = {
-        "e6478296-6135-47a0-8497-ae9b740bc804" = {
-          credentialsFile = "/home/kinzoku/.cloudflared/e6478296-6135-47a0-8497-ae9b740bc804.json";
+        "9a844696-e035-42d5-9ffb-27344db7d3b2" = {
+          credentialsFile = "/home/kinzoku/.cloudflared/9a844696-e035-42d5-9ffb-27344db7d3b2.json";
           default = "http_status:404";
           ingress = {
-            "ssh.the-nebula.xyz" = {
-              service = "ssh://localhost:22";
-            };
+            "ssh.the-nebula.xyz" = "ssh://localhost:22";
           };
         };
       };
     };
-    environment.systemPackages = [pkgs.cloudflared];
+    environment.systemPackages = [pkgs.cloudflared pkgs.cloudflare-warp];
 
-    virtualisation.arion.enable = true;
-    virtualisation.arion.projects.cloudflare.settings = {
-      project.name = "cloudflare";
-      services.cloudflare.service = {
-        image = "cloudflare/cloudflared:latest";
-        command = ["tunnel" "--no-autoupdate" "run" "--token" "${secrets.cloudflared-token.path}"];
-        volumes = [
-          "/home/${config.user.name}:/srv"
-        ];
-      };
-    };
+    # virtualisation.arion.enable = true;
+    # virtualisation.arion.projects.cloudflare.settings = {
+    #   project.name = "cloudflare";
+    #   services.cloudflare.service = {
+    #     image = "cloudflare/cloudflared:latest";
+    #     command = ["tunnel" "--no-autoupdate" "run" "--token" "${secrets.cloudflared-token.path}"];
+    #     volumes = [
+    #       "/home/${config.user.name}:/srv"
+    #     ];
+    #   };
+    # };
 
     users.users.${config.user.name}.extraGroups = ["docker"];
   };
