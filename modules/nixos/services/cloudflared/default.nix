6d1c24c9bb4ee55eb351535f924dfb2513962c67
kinzoku-dev <kinzokudev4869@gmail.com>
2023-11-30 17:50:55 -0600

    fix: make cloudflared arion use the actual token and not just the file path
    gosh im fucking retarded

--- modules/nixos/services/cloudflared/default.nix
+++ modules/nixos/services/cloudflared/default.nix
@@ -18,29 +18,17 @@ in {
     services.cloudflared = {
       enable = true;
       user = "kinzoku";
-      tunnels = {
-        "9a844696-e035-42d5-9ffb-27344db7d3b2" = {
-          credentialsFile = "/home/kinzoku/.cloudflared/9a844696-e035-42d5-9ffb-27344db7d3b2.json";
-          default = "http_status:404";
-          ingress = {
-            "ssh.the-nebula.xyz" = "ssh://localhost:22";
-          };
-        };
-      };
     };
     environment.systemPackages = [pkgs.cloudflared pkgs.cloudflare-warp];
 
-    # virtualisation.arion.enable = true;
-    # virtualisation.arion.projects.cloudflare.settings = {
-    #   project.name = "cloudflare";
-    #   services.cloudflare.service = {
-    #     image = "cloudflare/cloudflared:latest";
-    #     command = ["tunnel" "--no-autoupdate" "run" "--token" "${secrets.cloudflared-token.path}"];
-    #     volumes = [
-    #       "/home/${config.user.name}:/srv"
-    #     ];
-    #   };
-    # };
+    virtualisation.arion.enable = true;
+    virtualisation.arion.projects.cloudflare.settings = {
+      project.name = "cloudflare";
+      services.cloudflare.service = {
+        image = "cloudflare/cloudflared:latest";
+        command = ["tunnel" "--no-autoupdate" "run" "--token" "${builtins.readFile secrets.cloudflared-token.path}"];
+      };
+    };
 
     users.users.${config.user.name}.extraGroups = ["docker"];
   };
