1ea57752565c9fe536bc1ccb0103c0aef121082b
kinzoku-dev <kinzokudev4869@gmail.com>
2023-12-09 20:32:40 -0600

    tmux config stuff

--- modules/nixos/apps/tmux/default.nix
+++ modules/nixos/apps/tmux/default.nix
@@ -47,27 +47,34 @@ in {
         set -g mouse on
         set -g renumber-windows on
 
+        set -g status-position top
+
         set -g @catppuccin_flavour 'mocha'
-        set -g @catppuccin_window_left_separator "█"
-        set -g @catppuccin_window_right_separator "█ "
+        set -g @catppuccin_window_left_separator " █"
+        set -g @catppuccin_window_right_separator "█"
+        set -g @catppuccin_window_middle_separator " | "
         set -g @catppuccin_window_number_position "right"
-        set -g @catppuccin_window_middle_separator "  █"
 
-        set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_window_default_fill "all"
 
-        set -g @catppuccin_window_current_fill "number"
-        set -g @catppuccin_window_current_text "#{pane_current_path}"
+        set -g @catppuccin_window_current_fill "all"
 
-        set -g @catppuccin_status_modules_right "application session date_time"
+        set -g @catppuccin_status_modules_right "application session"
         set -g @catppuccin_status_left_separator  ""
-        set -g @catppuccin_status_right_separator " "
-        set -g @catppuccin_status_right_separator_inverse "yes"
+        set -g @catppuccin_status_right_separator " "
         set -g @catppuccin_status_fill "all"
         set -g @catppuccin_status_connect_separator "no"
       '';
       plugins = with pkgs; [
         tmuxPlugins.vim-tmux-navigator
-        tmuxPlugins.catppuccin
+        (tmuxPlugins.catppuccin.overrideAttrs (oldAttrs: {
+          src = oldAttrs.src.override {
+            owner = "kinzoku-dev";
+            repo = "catppuccin-tmux";
+            rev = "337d605a38fcefda48cef7dc61ddaa5b4f69687c";
+            hash = "sha256-gsOazBH5wQcwSoe/+f6t4HU4boypLVr0ySNAk4Tmvo8=";
+          };
+        }))
       ];
     };
   };
