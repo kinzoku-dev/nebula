f26f3dd01ed788bcde34c58c8191c8b1d655ea67
kinzoku-dev <kinzokudev4869@gmail.com>
2024-01-09 17:47:27 -0600

    0x0 instance

--- modules/nixos/services/0x0/default.nix
+++ modules/nixos/services/0x0/default.nix
@@ -12,4 +12,25 @@ in {
   options.OxO = with types; {
     enable = mkBoolOpt false "Enable 0x0";
   };
+
+  config = mkIf cfg.enable {
+    services.uwsgi = {
+      enable = true;
+      plugins = ["python3"];
+    };
+
+    systemd.services = {
+      "0x0" = {
+        description = "uWSGI instance to serve 0x0";
+        after = "network.target";
+        serviceConfig = {
+          User = "${config.user.name}";
+          Group = "www-data";
+          WorkingDirectory = "/home/${config.user.name}/0x0";
+          ExecStart = "uwsgi --plugin python3 --socket 0.0.0.0:9628 --protocol=http --wsgi-file fhost.py --callable app";
+        };
+        wantedBy = "multi-user.target";
+      };
+    };
+  };
 }
