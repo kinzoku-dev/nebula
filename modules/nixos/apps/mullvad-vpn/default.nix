4f71c4653f134f82649e0f10dff0222882bfdb0d
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-03-12 13:38:29 -0500

    feat(impermanence): add mullvad vpn directory to persist

--- modules/nixos/apps/mullvad-vpn/default.nix
+++ modules/nixos/apps/mullvad-vpn/default.nix
@@ -14,6 +14,9 @@ in {
   };
 
   config = mkIf cfg.enable {
+    system.persist.home.dirs = [
+      ".config/Mullvad VPN"
+    ];
     environment.systemPackages = [
       pkgs.mullvad-vpn
       (pkgs.makeDesktopItem {
