6abb4b756c1557c5423b09e7e9310096e7f2079d
kinzoku-dev <kinzokudev4869@gmail.com>
2024-03-21 19:02:37 -0500

    feat(nova): add some libs

--- systems/x86_64-linux/nova/default.nix
+++ systems/x86_64-linux/nova/default.nix
@@ -189,6 +189,11 @@
     in
       [
         inputs.nh.packages.x86_64-linux.default
+        libva
+        libglvnd
+        libGL
+        mesa
+        ffmpeg
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
         git
