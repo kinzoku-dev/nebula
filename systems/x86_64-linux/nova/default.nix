862e8be8c97175c5daff508721e849a1701df649
kinzoku-dev <kinzokudev4869@gmail.com>
2024-03-21 19:03:07 -0500

    feat(nova/apps): add localsend

--- systems/x86_64-linux/nova/default.nix
+++ systems/x86_64-linux/nova/default.nix
@@ -194,6 +194,9 @@
         libGL
         mesa
         ffmpeg
+
+        localsend
+
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
         wget
         git
