9a32b1da0e8a4380998dd92a3da76b0a46783000
kinzoku-dev <kinzokudev4869@gmail.com>
2024-03-21 19:03:46 -0500

    chore(gnupgModule): switch pinentry flavor option to new format

--- modules/nixos/system/security/gnupg/default.nix
+++ modules/nixos/system/security/gnupg/default.nix
@@ -19,7 +19,7 @@ in {
     services.pcscd.enable = true;
     programs.gnupg.agent = {
       enable = true;
-      pinentryFlavor = "curses";
+      pinentryPackage = pkgs.pinentry-curses;
       enableSSHSupport = true;
     };
 
