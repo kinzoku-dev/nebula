8af014aabaaabcd71101b6787194ab2935b6ce72
kinzoku-dev <kinzokudev4869@gmail.com>
2024-03-30 12:55:09 -0500

    style(users_module): format default.nix

--- modules/nixos/user/default.nix
+++ modules/nixos/user/default.nix
@@ -11,13 +11,9 @@ with lib.nebula; let
 in {
   options.user = with types; {
     name = mkOpt str "kinzoku" "The name of the user's account";
-    initialPassword =
-      mkOpt str "password123_*()"
-      "The initial password to use";
+    initialPassword = mkOpt str "password123_*()" "The initial password to use";
     extraGroups = mkOpt (listOf str) [] "Groups for the user to be assigned.";
-    extraOptions =
-      mkOpt attrs {}
-      "Extra options passed to users.users.<name>";
+    extraOptions = mkOpt attrs {} "Extra options passed to users.users.<name>";
   };
 
   config = {
@@ -31,7 +27,15 @@ in {
         group = "users";
 
         extraGroups =
-          ["wheel" "audio" "sound" "video" "networkmanager" "input" "tty"]
+          [
+            "wheel"
+            "audio"
+            "sound"
+            "video"
+            "networkmanager"
+            "input"
+            "tty"
+          ]
           ++ cfg.extraGroups;
       }
       // cfg.extraOptions;
