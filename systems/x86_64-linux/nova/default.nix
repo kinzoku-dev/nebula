a3ab06fe10ae7d205cad8135f50c9434c88cd0c2
kinzoku-dev <kinzokudev4869@gmail.com>
2024-04-12 12:58:37 -0500

    feat(packages)[nova]: add ansible

--- systems/x86_64-linux/nova/default.nix
+++ systems/x86_64-linux/nova/default.nix
@@ -205,6 +205,9 @@
         mesa
         ffmpeg
 
+        ansible
+        python3
+
         localsend
 
         vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
