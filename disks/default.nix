36186102640ff8b618bad0d6d2d4abe9163ee790
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-05-11 13:35:21 -0500

    feat(disko): add disk for steam deck

--- disks/default.nix
+++ disks/default.nix
@@ -1,7 +1,8 @@
 {device ? throw "Set this to your disk device, e.g. /dev/sda", ...}: {
   disko = {
     devices = {
-      disk.main = {
+      disk = {
+      main = {
         inherit device;
         type = "disk";
         content = {
@@ -39,7 +40,64 @@
           };
         };
       };
+      deck = {
+      	inherit device;
+	type = "disk";
+	content = {
+	  type = "gpt";
+	  partitions = {
+	    boot = {
+	      name = "boot";
+	      size = "1M";
+	      type = "EF02";
+	    };
+	    esp = {
+	      name = "ESP";
+	      size = "64M";
+	      type = "EF00";
+	      content = {
+	        type = "filesystem";
+		format = "vfat";
+	        mountpoint = "/boot";
+	      };
+	    };
+	    swap = {
+	      size = "4G";
+	      content = {
+	      	type = "swap";
+		resumeDevice = true;
+	      };
+	    };
+	    zfs = {
+	      size = "100%";
+	      content = {
+	        type = "zfs";
+		pool = "zroot2";
+	      };
+	    };
+	  };
+	};
+      };
+      };
       zpool = {
+      	zroot2 = {
+	  type = "zpool";
+	  mode = "mirror";
+	  rootFsOptions = {
+	    compression = "zstd";
+	    "com.sun:auto-snapshot" = "false";
+	  };
+	  mountpoint = "/";
+	  postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
+
+	  datasets = {
+	    nix = {
+	      type = "zfs_fs";
+	      mountpoint = "/nix";
+	      options.mountpoint = "legacy";
+	    };
+	  };
+	};
         zroot = {
           type = "zpool";
           rootFsOptions = {
