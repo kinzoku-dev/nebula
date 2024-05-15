96ba520e57877b3d82b0bbc8e6c31b0ca4eb6361
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-05-15 15:06:21 -0500

    feat(disko): Add some more disko disk configurations

--- disks/default.nix
+++ disks/default.nix
@@ -2,102 +2,102 @@
   disko = {
     devices = {
       disk = {
-      main = {
-        inherit device;
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              name = "boot";
-              size = "1M";
-              type = "EF02";
-            };
-            esp = {
-              name = "ESP";
-              size = "500M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
+        main = {
+          inherit device;
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                name = "boot";
+                size = "1M";
+                type = "EF02";
+              };
+              esp = {
+                name = "ESP";
+                size = "500M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              swap = {
+                size = "4G";
+                content = {
+                  type = "swap";
+                  resumeDevice = true;
+                };
+              };
+              root = {
+                size = "100%";
+                content = {
+                  type = "zfs";
+                  pool = "zroot";
+                };
               };
             };
-            swap = {
-              size = "4G";
-              content = {
-                type = "swap";
-                resumeDevice = true;
+          };
+        };
+        deck = {
+          inherit device;
+          type = "disk";
+          content = {
+            type = "gpt";
+            partitions = {
+              boot = {
+                name = "boot";
+                size = "1M";
+                type = "EF02";
               };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "zfs";
-                pool = "zroot";
+              esp = {
+                name = "ESP";
+                size = "64M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                };
+              };
+              swap = {
+                size = "4G";
+                content = {
+                  type = "swap";
+                  resumeDevice = true;
+                };
+              };
+              zfs = {
+                size = "100%";
+                content = {
+                  type = "zfs";
+                  pool = "zroot2";
+                };
               };
             };
           };
         };
       };
-      deck = {
-      	inherit device;
-	type = "disk";
-	content = {
-	  type = "gpt";
-	  partitions = {
-	    boot = {
-	      name = "boot";
-	      size = "1M";
-	      type = "EF02";
-	    };
-	    esp = {
-	      name = "ESP";
-	      size = "64M";
-	      type = "EF00";
-	      content = {
-	        type = "filesystem";
-		format = "vfat";
-	        mountpoint = "/boot";
-	      };
-	    };
-	    swap = {
-	      size = "4G";
-	      content = {
-	      	type = "swap";
-		resumeDevice = true;
-	      };
-	    };
-	    zfs = {
-	      size = "100%";
-	      content = {
-	        type = "zfs";
-		pool = "zroot2";
-	      };
-	    };
-	  };
-	};
-      };
-      };
       zpool = {
-      	zroot2 = {
-	  type = "zpool";
-	  mode = "mirror";
-	  rootFsOptions = {
-	    compression = "zstd";
-	    "com.sun:auto-snapshot" = "false";
-	  };
-	  mountpoint = "/";
-	  postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
+        zroot2 = {
+          type = "zpool";
+          mode = "mirror";
+          rootFsOptions = {
+            compression = "zstd";
+            "com.sun:auto-snapshot" = "false";
+          };
+          mountpoint = "/";
+          postCreateHook = "zfs list -t snapshot -H -o name | grep -E '^zroot@blank$' || zfs snapshot zroot@blank";
 
-	  datasets = {
-	    nix = {
-	      type = "zfs_fs";
-	      mountpoint = "/nix";
-	      options.mountpoint = "legacy";
-	    };
-	  };
-	};
+          datasets = {
+            nix = {
+              type = "zfs_fs";
+              mountpoint = "/nix";
+              options.mountpoint = "legacy";
+            };
+          };
+        };
         zroot = {
           type = "zpool";
           rootFsOptions = {
