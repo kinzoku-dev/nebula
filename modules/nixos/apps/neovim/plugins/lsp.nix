9e12a830ce3e2d3cbd4531e7ae9e27bd17fca390
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-05-15 15:07:48 -0500

    feat(neovim): Add yamlls

--- modules/nixos/apps/neovim/plugins/lsp.nix
+++ modules/nixos/apps/neovim/plugins/lsp.nix
@@ -49,6 +49,9 @@
         servers = {
           ansiblels.enable = true;
           bashls.enable = true;
+          yamlls = {
+            enable = true;
+          };
           helm-ls.enable = true;
           terraformls.enable = true;
           clangd.enable = true;
@@ -237,6 +240,28 @@
       require('lspconfig.ui.windows').default_options = {
           border = _border
       }
+
+      require('lspconfig').yamlls.setup({
+        settings = {
+          yaml = {
+            schemas = {
+              kubernetes = "*.yaml",
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*",
+              ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+              ["http://json.schemastore.org/prettierrc"] = ".prettierrc.{yml,yaml}",
+              ["http://json.schemastore.org/kustomization"] = "{kustomization,ks}.{yml,yaml}",
+              ["http://json.schemastore.org/ansible-playbook"] = "*play*.{yml,yaml}",
+              ["http://json.schemastore.org/chart"] = "Chart.{yml,yaml}",
+              ["https://json.schemastore.org/dependabot-v2"] = ".github/dependabot.{yml,yaml}",
+              ["https://json.schemastore.org/gitlab-ci"] = "*gitlab-ci*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json"] = "*api*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "*docker-compose*.{yml,yaml}",
+              ["https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json"] = "*flow*.{yml,yaml}",
+            },
+          }
+        }
+      })
     '';
   };
 }
