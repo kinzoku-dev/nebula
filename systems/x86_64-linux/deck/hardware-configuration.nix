2f33faf9c5eec765b4f9f1104858181525c9a846
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-06-07 10:30:48 -0500

    fix(deck): change hardware.cpu.intel.updateMicrocode to hardware.cpu.amd.updateMicrocode

--- systems/x86_64-linux/deck/hardware-configuration.nix
+++ systems/x86_64-linux/deck/hardware-configuration.nix
@@ -27,5 +27,5 @@
   # networking.interfaces.eno1.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
