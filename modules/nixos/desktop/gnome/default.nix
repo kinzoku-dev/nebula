f151b671633dd7688502a8946a9c815de39b5f68
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-06-12 14:34:28 -0500

    feat(desktop): add more stuff for gnome

--- modules/nixos/desktop/gnome/default.nix
+++ modules/nixos/desktop/gnome/default.nix
@@ -15,5 +15,15 @@ in {
 
   config = mkIf cfg.enable {
     services.xserver.desktopManager.gnome.enable = true;
+    home.extraOptions.dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    environment.systemPackages = with pkgs; [
+      gnomeExtensions.appindicator
+    ];
+    services.udev.packages = with pkgs; [gnome.gnome-settings-daemon];
+    services.gnome = {
+      core-utilities.enable = true;
+      core-shell.enable = true;
+      core-os-services.enable = true;
+    };
   };
 }
