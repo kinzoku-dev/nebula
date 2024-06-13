a5ca8d42a634eb07f1970268517588aa07a0a76b
kinzoku-dev <kinzoku@the-nebula.xyz>
2024-06-13 13:45:47 -0500

    feat(apps/gaming): format some stuff in gaming module

--- modules/nixos/apps/gaming/default.nix
+++ modules/nixos/apps/gaming/default.nix
@@ -14,85 +14,89 @@ in {
   };
 
   config = mkIf cfg.enable {
-    programs.steam = {
-      enable = true;
-      package =
-        (pkgs.steam.override {
-          extraPkgs = pkgs:
-            with pkgs; [
-              xorg.libXcursor
-              xorg.libXi
-              xorg.libXinerama
-              xorg.libXScrnSaver
-              libpng
-              libpulseaudio
-              libvorbis
-              libGL
-              stdenv.cc.cc.lib
-              libkrb5
-              keyutils
-              libgdiplus
-              at-spi2-atk
-              fmodex
-              gtk3
-              gtk3-x11
-              harfbuzz
-              icu
-              glxinfo
-              inetutils
-              libthai
-              mono5
-              pango
-              strace
-              zlib
-              iconv
-              libunwind
+    programs = {
+      steam = {
+        enable = true;
+        package =
+          (pkgs.steam.override {
+            extraPkgs = pkgs:
+              with pkgs; [
+                xorg.libXcursor
+                xorg.libXi
+                xorg.libXinerama
+                xorg.libXScrnSaver
+                libpng
+                libpulseaudio
+                libvorbis
+                libGL
+                stdenv.cc.cc.lib
+                libkrb5
+                keyutils
+                libgdiplus
+                at-spi2-atk
+                fmodex
+                gtk3
+                gtk3-x11
+                harfbuzz
+                icu
+                glxinfo
+                inetutils
+                libthai
+                mono5
+                pango
+                strace
+                zlib
+                iconv
+                libunwind
+              ];
+          })
+          .overrideAttrs
+          {
+            desktopItems = [
+              (pkgs.makeDesktopItem {
+                name = "Steam";
+                desktopName = "Steam";
+                exec = "${lib.optionalString config.apps.mullvad-vpn.enable "mullvad-exclude"} ${lib.optionalString config.hardware.graphics.nvidiaOffload.enable "prime-run"} steam";
+                icon = "steam";
+                startupWMClass = "Signal Beta";
+                genericName = "Internet Messenger";
+                keywords = [
+                  "call"
+                  "signal"
+                  "electron"
+                  "chat"
+                ];
+                categories = [
+                  "Network"
+                  "InstantMessaging"
+                  "Chat"
+                ];
+              })
             ];
-        })
-        .overrideAttrs
-        {
-          desktopItems = [
-            (pkgs.makeDesktopItem {
-              name = "Steam";
-              desktopName = "Steam";
-              exec = "${lib.optionalString config.apps.mullvad-vpn.enable "mullvad-exclude"} ${lib.optionalString config.hardware.graphics.nvidiaOffload.enable "prime-run"} steam";
-              icon = "steam";
-              startupWMClass = "Signal Beta";
-              genericName = "Internet Messenger";
-              keywords = [
-                "call"
-                "signal"
-                "electron"
-                "chat"
-              ];
-              categories = [
-                "Network"
-                "InstantMessaging"
-                "Chat"
-              ];
-            })
-          ];
-        };
-      # remotePlay.openFirewall = true;
-      # dedicatedServer.openFirewall = true;
-      gamescopeSession.enable = true;
-    };
+          };
+        # remotePlay.openFirewall = true;
+        # dedicatedServer.openFirewall = true;
+        gamescopeSession.enable = true;
+      };
 
-    programs.gamemode.enable = true;
+      gamemode.enable = true;
+    };
     system.persist.home.dirs = [
       ".local/share/Steam"
       ".local/share/Terraria"
     ];
-    environment.systemPackages = with pkgs; [
-      protonup
-      mangohud
-      goverlay
-      lutris
-      heroic
-      bottles
-    ];
-    environment.sessionVariables = {
-      STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/${config.user.name}/.steam/root/compatibilitytools.d";
+    environment = {
+      systemPackages = with pkgs; [
+        protonup
+        mangohud
+        goverlay
+        lutris
+        heroic
+        bottles
+      ];
+      sessionVariables = {
+        STEAM_EXTRA_COMPAT_TOOLS_PATHS = "/home/${config.user.name}/.steam/root/compatibilitytools.d";
+      };
     };
   };
 }
